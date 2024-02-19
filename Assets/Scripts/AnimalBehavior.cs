c2957f0b295b99ea89eaea3445623000ca6da26f
Dinofox12 <145399038+Dinofox12@users.noreply.github.com>
2024-02-18 19:51:34 -0500

    Roy
    did stuff

--- Assets/Scripts/AnimalBehavior.cs
+++ Assets/Scripts/AnimalBehavior.cs
@@ -3,17 +3,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
+using TMPro;
+
+public class AnimalBehavior : MonoBehaviour
+{
+    public TMP_Text Text;
+    int number;
+    string name;
+    private void update() 
+    {
+        ButtonPromptManager button = GetComponent<ButtonPromptManager>();
+        Animal[] animal = { new Moose(), new Gorilla(), new Tortoise(), new Monkey() };
+        if (button.getNumber(number)>=0)
+        {
+            Text.text = "animal Name:" + animal[number].animalName(name); "Amount:" + animal[number].animalAmount() + "Description" + animal[number].animalDescription() + "Sound" + animal[number].animalSound();
+        }
+    }
+
+}
+
+
+
+
 
 public interface IInteractable
 {
     void animalInteract();
 }
 
+public interface IFeedable
+{
+    void FeedAnimal();
+}
+
 // Abstract Base Class
 public abstract class Animal
 {
     public abstract int animalAmount(int animalAmount);
-    public abstract string animalName(string name);
+    public abstract void animalName(string name);
     public abstract string animalDescription(string description);
     public abstract string animalSound(string sound);
 }
@@ -24,14 +51,20 @@ public class Moose : Animal
     {
         Debug.Log("Moose is being interacted with!");
     }
+
+    public void FeedAnimal()
+    {
+        Debug.Log("Moose has been fed!");
+    }
+
     public override int animalAmount(int animalAmount)
     {
         return 4;
     }
 
-    public override string animalName(string name)
+    public override void animalName(string name)
     {
-        return "Moose (Meese)";
+        name = "Moose (Meese)";
     }
 
     public override string animalDescription(string description)
@@ -51,14 +84,20 @@ public class Gorilla : Animal
     {
         Debug.Log("Gorilla is being interacted with!");
     }
+
+    public void FeedAnimal()
+    {
+        Debug.Log("Gorilla has been fed!");
+    }
+
     public override int animalAmount(int animalAmount)
     {
         return 4;
     }
 
-    public override string animalName(string name)
+    public override void animalName(string name)
     {
-        return "Silverback Gorilla";
+        name =  "Silverback Gorilla";
     }
 
     public override string animalDescription(string description)
@@ -76,17 +115,22 @@ public class Tortoise : Animal
 {
     public void Interact()
     {
-
         Debug.Log("Tortoise is being interacted with!");
     }
+
+    public void FeedAnimal()
+    {
+        Debug.Log("Tortoise has been fed!");
+    }
+
     public override int animalAmount(int animalAmount)
     {
         return 5;
     }
 
-    public override string animalName(string name)
+    public override void animalName(string name)
     {
-        return "Tortoise";
+        name = "Tortoise";
     }
 
     public override string animalDescription(string description)
@@ -106,14 +150,20 @@ public class Monkey : Animal
     {
         Debug.Log("Monkey is being interacted with!");
     }
+
+    public void FeedAnimal()
+    {
+        Debug.Log("Monkey has been fed!");
+    }
+
     public override int animalAmount(int animalAmount)
     {
         return 6;
     }
 
-    public override string animalName(string name)
+    public override void animalName(string name)
     {
-        return "Monkeys";
+        name = "Monkeys";
     }
 
     public override string animalDescription(string description)
--- Assets/Scripts/ButtonPrompt.cs
+++ Assets/Scripts/ButtonPrompt.cs
@@ -6,15 +6,21 @@ public class ButtonPromptManager : MonoBehaviour
     public Transform[] signTransforms; 
     public GameObject[] promptObjects; 
     public float activationDistance = 200f;
-
+    public int animalnumber;
+    public int getNumber(int number)
+    {
+        return animalnumber;
+    }
     void Update()
     {
+        animalnumber = -1;
         for (int i = 0; i < signTransforms.Length; i++)
         {
             float distance = Vector2.Distance(playerTransform.position, signTransforms[i].position);
             if (distance <= activationDistance)
             {
                 promptObjects[i].SetActive(true);
+                animalnumber = i;
             }
             else
             {
